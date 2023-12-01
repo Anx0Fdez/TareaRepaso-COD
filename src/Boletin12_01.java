d04c11f7b0e5836a4b604cd4928d9fe048b5394e
Anxo Fdez Rguez <git config --global user.email>
2023-12-01 22:30:43 +0100

    Boletin12.v1

---
+++ src/Boletin12_01.java
@@ -0,0 +1,21 @@
+
+/**
+ * Clase Main
+ * @author Anxo Fdez
+ * @version v1.0
+ */
+
+public class Boletin12_01 {
+
+    /**
+     * Crea un objeto Garage para utilizar los métodos de la clase
+     *
+     * @param args no recibe parámetros
+     */
+
+    public static void main(String[] args) {
+        Garaxe g = new Garaxe();
+        g.comprobarPlazas();
+        g.calcularPrezo();
+    }
+}
---
+++ src/Coche.java
@@ -0,0 +1,26 @@
+
+/**
+ * @author Anxo Fdez
+ * @version v1.0
+ */
+public class Coche {
+
+    private String matricula;
+    private String marca;
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+}
